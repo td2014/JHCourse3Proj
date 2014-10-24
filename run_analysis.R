33c5f417c0084cdc66548631cc89a95107c2a7d2
td2014 <anthonyd2004@gmail.com>
2014-10-24 13:40:46 -0700

    First commit

---
+++ run_analysis.R
@@ -0,0 +1,41 @@
+#
+# run_analysis.R
+#
+# Script to perform processing for Coursera Data Science Class.
+# Oct 24, 2014
+#
+#
+
+#
+# Clear workspace
+#
+rm(list = ls())
+library(plyr) # based on "tidy data" readings from course
+
+#
+# Step 1: Merges the training and the test sets to create one data set.
+#
+
+trainData = read.csv
+
+#
+# Step 2: Extracts only the measurements on the mean and standard deviation for each measurement. 
+#
+
+#
+# Step 3: Uses descriptive activity names to name the activities in the data set
+#
+
+#
+# Step 4: Appropriately labels the data set with descriptive variable names. 
+#
+
+#
+# Step 5: From the data set in step 4, creates a second, 
+# independent tidy data set with the average of each variable for each activity and each subject.
+#
+
+
+#
+# End of script
+#
